f0f411d340867a362fa0a65d12db5577bf49f7e7
Suraj Prajapati <119839665+Suraj-Prajapati26@users.noreply.github.com>
2023-04-29 12:18:27 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -4,9 +4,9 @@ session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('1065293136026-m69v8npcc6826ejod57q8jj3b6cdvnhr.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/zzz/hello.php');
+$client->setClientId('YOUR-CLIENT-ID');
+$client->setClientSecret('YOUR-CLIENT-SECRET');
+$client->setRedirectUri('REDIRECT-URI');
 $client->addScope(Google_Service_Drive::DRIVE_FILE);
 
 if (isset($_GET['code'])) {
